ab5903adfd09bf93cada3d83bc36eb356576469e
Stefan Tauner <stefan.tauner@gmx.at>
2013-03-26 02:13:05 +0100

    Use realtimeify

--- main.c
+++ main.c
@@ -1,29 +1,18 @@
-/*
- * TODO:
- *		- get current time
- * 		- sync µc
- * 		- schedule event at precisely x ns
- * 			# scheduling
- * 			# cpu affinity
- * 		- toggle some pin
- */
-
-#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include <errno.h>
 #include <inttypes.h>
 #include <unistd.h>
 #include <string.h>
 #include <sys/io.h>
-#include <sched.h>
 #include <time.h>
+#include "realtimeify.h"
 #include "j2a.h"
 
 #define uint unsigned int
-#define THIS_PROC	0
-#define NANO 1000000000L
+#define NS_PER_SEC 1000000000L
+#define WAKEUP_HEADROOM_NS 10000 /* number of ns to wake up before event */
+#define PRECISION_NS 500 /* execute only if in interval [event-PRECISION_NS; event) */
 
 int port80(void) {
 	while(true) {
@@ -39,47 +28,6 @@ int port80(void) {
 		//
 	//}
 	return 0;
-}
-
-int set_affin(int cpu) {
-	cpu_set_t mask;
-
-	// FIXME: input validation
-
-	CPU_ZERO(&mask);
-	CPU_SET(cpu, &mask);
-
-	if(sched_setaffinity(THIS_PROC, sizeof(cpu_set_t), &mask) != 0) {
-		fprintf(stderr, "sched_setaffinity error %d: %s\n", errno, strerror(errno));
-		return EXIT_FAILURE;
-	}
-
-	printf("Running on CPU #%d now.\n", cpu);
-	return EXIT_SUCCESS;
-}
-
-int set_sched(int policy) {
-	struct sched_param param;
-
-	if(sched_getparam(THIS_PROC, &param) < 0) {
-		fprintf(stderr, "sched_get_priority_min error %d: %s\n", errno, strerror(errno));
-		return EXIT_FAILURE;
-	}
-
-	param.sched_priority = sched_get_priority_min(policy);
-
-	if(param.sched_priority < 0) {
-		fprintf(stderr, "sched_get_priority_min error %d: %s\n", errno, strerror(errno));
-		return EXIT_FAILURE;
-	}
-
-	if(sched_setscheduler(THIS_PROC, policy, &param) != 0) {
-		fprintf(stderr, "sched_setscheduler error %d: %s\n", errno, strerror(errno));
-		return EXIT_FAILURE;
-	}
-
-	printf("Running with policy %d at priority %d now.\n", policy, param.sched_priority);
-	return EXIT_SUCCESS;
 }
 
 /* buf needs to be able to store a maximum of 30 characters. */
@@ -127,7 +75,7 @@ int get_time(int clk_id, const char *clk_name) {
 		printf("timespec2str failed!\n");
 		return EXIT_FAILURE;
 	} else {
-		unsigned long resol = res.tv_sec * NANO + res.tv_nsec;
+		unsigned long resol = res.tv_sec * NS_PER_SEC + res.tv_nsec;
 		printf("%s: res=%ld ns, time=%s\n", clk_name, resol, timestr);
 		return EXIT_SUCCESS;
 	}
@@ -198,20 +146,8 @@ int sync_time(struct j2a_handle *comm) {
 	}
 	return 0;
 }
-int main(int argc, char **argv) {
-	get_time(CLOCK_REALTIME, 			"CLOCK_REALTIME          ");
-	get_time(CLOCK_MONOTONIC, 			"CLOCK_MONOTONIC         ");
-	get_time(CLOCK_MONOTONIC_RAW, 		"CLOCK_MONOTONIC_RAW     ");
-	get_time(CLOCK_PROCESS_CPUTIME_ID,	"CLOCK_PROCESS_CPUTIME_ID");
-	get_time(CLOCK_THREAD_CPUTIME_ID,	"CLOCK_THREAD_CPUTIME_ID ");
 
-	if(iopl(3) != 0) {
-		fprintf(stderr, "ERROR: Could not get I/O privileges (%s).\n"
-						"You need to be root.\n", strerror(errno));
-		return 1;
-	}
-	set_affin(sysconf(_SC_NPROCESSORS_ONLN)/2);
-	set_sched(SCHED_RR);
+int benchmark(int argc, char **argv) {
 
 	if (j2a_init() != 0)
 		return EXIT_FAILURE;
@@ -238,9 +174,15 @@ int main(int argc, char **argv) {
 		if (j2a_send_by_name(comm, &p, argv[1]) == 0)
 			j2a_packet_print(&p);
 	} else
-		return sync_time(comm);
+		sync_time(comm);
+
 	j2a_disconnect(comm);
 	j2a_shutdown();
 
 	return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+	return realtimeify(benchmark, argc, argv);
+	//return benchmark(argc, argv);
 }
--- makefile
+++ makefile
@@ -1,11 +1,12 @@
 SHELL = /bin/sh
 CWD := $(shell pwd)
-CFLAGS ?= -Wall -pedantic -O3 -std=gnu99
-CPPFLAGS ?= -I java2arduino-c
+CFLAGS ?= -Wall -pedantic -O3 -std=gnu99 -g -Wno-unused-function
+CPPFLAGS ?= -I java2arduino-c -I realtimeify
 LDFLAGS ?= -lrt -lusb-1.0
 SRCS := \
 	$(wildcard *.c) \
-	$(wildcard java2arduino-c/*.c)
+	$(wildcard java2arduino-c/*.c) \
+	$(wildcard realtimeify/*.c)
 OBJS = $(SRCS:%.c=%.o)
 DEPS = $(SRCS:%.c=%.d)
 EXE := main.exe
