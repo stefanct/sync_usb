c6dab54988fe69f48023d2c2583af835cd3420b5
Stefan Tauner <stefan.tauner@gmx.at>
2013-03-30 01:08:01 +0100

    Refactor main.exe
    - Split get_time() into print_timespec(), print_time_device() and print_time_host()
    - Get rid of sync_time()
    - Allow for real precise wakeups in benchmark()

--- main.c
+++ main.c
@@ -12,8 +12,7 @@
 
 #define uint unsigned int
 #define NS_PER_SEC 1000000000L
-#define WAKEUP_HEADROOM_NS 10000 /* number of ns to wake up before event */
-#define PRECISION_NS 500 /* execute only if in interval [event-PRECISION_NS; event) */
+#define US_PER_SEC    1000000L
 
 int port80(void) {
 	while(true) {
@@ -64,66 +63,89 @@ int timespec2str(char *buf, uint len, struct timespec *ts) {
 	return 0;
 }
 
-int get_time(int clk_id, const char *clk_name) {
+int print_timespec(struct timespec *ts, struct timespec *res) {
 	const uint TIME_FMT = strlen("2012-12-31 12:59:59.123456789") + 1;
 	char timestr[TIME_FMT];
-
-	struct timespec ts, res;
-	clock_getres(clk_id, &res);
-	clock_gettime(clk_id, &ts);
-
-	if (timespec2str(timestr, TIME_FMT, &ts) != 0) {
+	if (timespec2str(timestr, TIME_FMT, ts) != 0) {
 		printf("timespec2str failed!\n");
 		return EXIT_FAILURE;
 	} else {
-		unsigned long resol = res.tv_sec * NS_PER_SEC + res.tv_nsec;
-		printf("%s: res=%ld ns, time=%s\n", clk_name, resol, timestr);
+		unsigned long resol = (res != NULL) ? resol = res->tv_sec * NS_PER_SEC + res->tv_nsec : 0;
+		printf("res=%ld ns, time=%s\n", resol, timestr);
 		return EXIT_SUCCESS;
 	}
 }
 
-int sync_time(struct j2a_handle *comm) {
-	struct j2a_packet p = {0};
-	p.len = 10; // 64 + 16 bits = 10 B
-	uint64_t *timebuf = (uint64_t *)&p.msg[0];
-	uint16_t *framebuf = (uint16_t *)&p.msg[8];
+int print_time_device(struct j2a_handle *comm) {
+	struct j2a_packet p;
+	p.len = 0;
+	if (j2a_send_by_name(comm, &p, "getTime") != 0)
+		return EXIT_FAILURE;
 
-	while (true) {
-		// wait for and get the new frame
-		int fn = wait_for_new_frame_number();
-		if (fn < 0)
-			return 1;
-		*framebuf = fn;
-		// get timestamp
-		struct timespec ts;
-		if (clock_gettime(CLOCK_REALTIME, &ts) != 0)
-			return 2;
-		uint64_t time = ts.tv_sec * 1000000 + ts.tv_nsec / 1000;
-		*timebuf = time;
-		p.len = 10;
-		printf("sent timestamp=%" PRId64 " 0x%016" PRIx64 ", framenumber=%04" PRIx16 "\n", time, time, fn);
-		//j2a_packet_print(&p);
-		if (j2a_send_by_name(comm, &p, "syncTime") != 0)
-			return 3;
-		p.len=0;
-		if (j2a_send_by_name(comm, &p, "getTime") != 0)
-			return 4;
-		printf("rcvd timestamp=%" PRId64 " 0x%016" PRIx64 " (fn=%04" PRIx16 "\n", *timebuf, *timebuf, *framebuf);
-		sleep(5);
-	}
-	return 0;
+	j2a_packet_print(&p);
+	uint64_t us = 0;
+	for (unsigned int i = 0; i < 8; i++)
+		us |= (uint64_t)p.msg[i] << (i * 8);
+
+	printf("us=%0"PRIx64"\n", us);
+	printf("us=%0"PRId64"\n", us);
+
+	struct timespec ts;
+	ts.tv_nsec = us % US_PER_SEC * 1000;
+	ts.tv_sec = us / US_PER_SEC;
+	printf("sec=%0"PRId64"\n", ts.tv_sec);
+	return print_timespec(&ts, NULL);
+	
+	//return EXIT_SUCCESS;
 }
 
-int benchmark(int argc, char **argv) {
+int print_time_host(int clk_id, const char *clk_name) {
 
-	if (j2a_init() != 0)
+	struct timespec ts, res;
+	clock_getres(clk_id, &res);
+	clock_gettime(clk_id, &ts);
+	return print_timespec(&ts, &res);
+}
+
+#define WAKEUP_HEADROOM_NS 10000 /* number of ns to wake up before event */
+#define PRECISION_NS 500 /* execute only if in interval [event-PRECISION_NS; event) */
+
+int benchmark(int argc, char **argv) {
+	struct timespec sync_ts, ts;
+	clock_gettime(CLOCK_REALTIME, &sync_ts);
+	sync_ts.tv_nsec = NS_PER_SEC - 10000;
+	for (unsigned int i = 0; i < 10; i++) {
+		sync_ts.tv_sec++;
+		clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &sync_ts, NULL);
+		while (true) {
+			clock_gettime(CLOCK_REALTIME, &ts);
+			if (ts.tv_nsec >= NS_PER_SEC - PRECISION_NS)
+				break;
+			if (ts.tv_nsec < sync_ts.tv_nsec) {
+				sync_ts.tv_sec++;
+				clock_nanosleep(CLOCK_REALTIME, TIMER_ABSTIME, &sync_ts, NULL);
+			}
+		}
+		char buf[30];
+		timespec2str(buf, sizeof(buf), &ts);
+		printf("%s\n", buf);
+	}
+	return EXIT_SUCCESS;
+}
+
+int main(int argc, char **argv) {
+	//return realtimeify(benchmark, argc, argv);
+	if (j2a_init() != 0) {
+		fprintf(stderr, "init failed\n");
 		return EXIT_FAILURE;
+	}
 
 	//libusb_set_debug(NULL, 2);
 	struct j2a_handle *comm = j2a_connect(NULL);
-	if (comm == NULL)
+	if (comm == NULL) {
+		fprintf(stderr, "connect failed\n");
 		return EXIT_FAILURE;
-
+	}
 	printf("connected.\n");
 	if (j2a_fetch_funcmap(comm) != 0)
 		return EXIT_FAILURE;
@@ -135,21 +157,18 @@ int benchmark(int argc, char **argv) {
 	
 	j2a_print_propmap(comm, stdout);
 
+	int ret = EXIT_SUCCESS;
 	if (argc > 1) {
 		struct j2a_packet p;
 		p.len = 0;
-		if (j2a_send_by_name(comm, &p, argv[1]) == 0)
+		if ((ret = j2a_send_by_name(comm, &p, argv[1])) == 0)
 			j2a_packet_print(&p);
-	} else
-		sync_time(comm);
+	} else {
+		ret = print_time_device(comm);
+	}
 
 	j2a_disconnect(comm);
 	j2a_shutdown();
 
-	return EXIT_SUCCESS;
-}
-
-int main(int argc, char **argv) {
-	return realtimeify(benchmark, argc, argv);
-	//return benchmark(argc, argv);
+	return ret;
 }
